667599f5090528bdf5c7dfb017a8e3395b2ac86e
Ivan Oleynikov <ivan.oleynikov95@gmail.com>
2024-12-07 03:26:58 +0100

    update

--- modules/boards/orangepi5plus.nix
+++ modules/boards/orangepi5plus.nix
@@ -50,7 +50,7 @@ in {
           / {
             compatible = "rockchip,rk3588-orangepi-5-plus";
             fragment@0 {
-              target = "/base/hdmirx-controller@fdee0000";
+              target = <&hdmirx_ctrlerr>;
               __overlay__ {
                 status = "disabled";
               };
