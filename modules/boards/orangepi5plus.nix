5f2d18f782aafbdec01933239e29860f5f9b11a4
Ivan Oleynikov <ivan.oleynikov95@gmail.com>
2024-12-07 03:22:07 +0100

    update

--- modules/boards/orangepi5plus.nix
+++ modules/boards/orangepi5plus.nix
@@ -48,7 +48,7 @@ in {
           /plugin/;
 
           / {
-            compatible;
+            compatible = "rockchip,rk3588-orangepi-5-plus";
             fragment@0 {
               target = <&hdmirx_ctrler>;
               __overlay__ {
