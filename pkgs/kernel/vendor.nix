fe000c35a1ed3e12ad0aab15205b55a612fa422e
Ivan Oleynikov <ivan.oleynikov95@gmail.com>
2025-03-18 16:13:39 +0500

    Revert "joshua riek kernel"
    This reverts commit 407be60e39aae59960d604356709114617b76ec2.

--- pkgs/kernel/vendor.nix
+++ pkgs/kernel/vendor.nix
@@ -14,19 +14,19 @@
 , ...
 }:
 let
-  modDirVersion = "6.1.75";
+  modDirVersion = "6.1.99";
 in
 (linuxManualConfig {
   inherit modDirVersion;
-  version = "${modDirVersion}-riek";
+  version = "${modDirVersion}-armbian";
   extraMeta.branch = "6.1";
 
   # https://github.com/Joshua-Riek/linux-rockchip/tree/noble
   src = fetchFromGitHub {
-    owner = "Joshua-Riek";
+    owner = "armbian";
     repo = "linux-rockchip";
-    rev = "noble";
-    hash = "sha256-m0tDapnAUYnZPNVaqrGUdGBF2sYCrZclCroJ3X6etkQ=";
+    rev = "rk-6.1-rkr5";
+    hash = "sha256-TPOUGUjvaKP5SglVomI8y7AKN1QbL735MnQQQxsisVA=";
   };
 
   # https://github.com/hbiyik/linux/tree/rk-6.1-rkr3-panthor
