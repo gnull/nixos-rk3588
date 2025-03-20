df295756f07f3a11a279ad55a37d62c6458fc3ac
Ivan Oleynikov <ivan.oleynikov95@gmail.com>
2025-03-20 14:13:14 +0500

    arm64 defconfig

--- pkgs/kernel/vendor.nix
+++ pkgs/kernel/vendor.nix
@@ -41,7 +41,7 @@ in
   #  5. Then use `make menuconfig` in kernel's root directory to view and customize the kernel(like enable/disable rknpu, rkflash, ACPI(for UEFI) etc).
   #  6. copy the generated .config to ./pkgs/kernel/rk35xx_vendor_config (also be sure to update the corresponding `.nix` file accordingly) and commit it.
   # 
-  configfile = "${src}/arch/arm/configs/rockchip_linux_defconfig";
+  configfile = "${src}/arch/arm64/configs/rockchip_linux_defconfig";
   # config = import ./rk35xx_vendor_config.nix;
 }).overrideAttrs (old: {
   name = "k"; # dodge uboot length limits
